c118f268be874865ff0050b28221f010421ec2b0
jiyongmoon <asdwldyd123@gmail.com>
2023-07-13 10:30:54 +0900

    2023.07.13

---
+++ book/05.이진탐색/04.이진탐색-기출문제28.py
@@ -0,0 +1,51 @@
+# 고정점 찾기
+
+## 나의 답안 ##
+
+n = int(input())
+
+array = list(map(int, input().split()))
+
+def search(array, start, end):
+    if start > end:
+        return -1
+    
+    mid = (start + end) // 2
+    
+    if array[mid] == mid:
+        return mid
+
+    elif array[mid] > mid:
+        return search(array, start, mid-1)
+    
+    else:
+        return search(array, mid+1, end)
+    
+print(search(array, 0, n-1))
+
+## 예시 답안 ##
+
+def binary_search(array, start, end):
+    if start > end:
+        return -1
+    
+    mid = (start + end) // 2
+    
+    if array[mid] == mid:
+        return mid
+
+    elif array[mid] > mid:
+        return binary_search(array, start, mid-1)
+    
+    else:
+        return binary_search(array, mid+1, end)
+    
+n = int(input())
+array = list(map(int, input().split()))    
+
+index = binary_search(array, 0, n-1)
+
+if index == None:
+    print(-1)
+else:
+    print(index)
