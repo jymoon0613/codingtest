f8a2b009649280379fe4fefba610c05c84c109d7
jiyongmoon <asdwldyd123@gmail.com>
2023-08-25 13:44:15 +0900

    2023.08.25 최단경로

--- book/07.최단경로/01.최단경로-실전문제01.py
+++ book/07.최단경로/01.최단경로-실전문제01.py
@@ -2,54 +2,35 @@
 
 ## 나의 답안 ##
 
-import heapq
-
-INF = int(1e+9)
 n, m = map(int, input().split())
 
-distance = [INF] * (n+1)
-graph = [[] for _ in range(n+1)]
+INF = int(1e+9)
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            graph[i][j] = 0
 
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a].append((b, 1))
-    graph[b].append((a, 1))
+    graph[a][b] = 1
+    graph[b][a] = 1
 
-x, k = map(int, input().split())
+target1, target2 = map(int, input().split())
 
-def search(start):
-    
-    q = []
-    heapq.heappush(q, (0, start))
-    
-    distance[start] = 0
-        
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-                
-search(1)
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
-tok = distance[k]
+dist1 = graph[1][target2]
+dist2 = graph[target2][target1]
 
-distance = [INF] * (n+1)
-
-search(k)
-
-tox = distance[x]
-
-res = tok + tox
-
-if tox == INF:
+if dist1 == INF or dist2 == INF:
     print(-1)
 else:
-    print(res)
+    print(dist1 + dist2)
 
 ## 예시 답안 ##
 
--- book/07.최단경로/02.최단경로-실전문제02.py
+++ book/07.최단경로/02.최단경로-실전문제02.py
@@ -8,45 +8,45 @@ INF = int(1e+9)
 
 n, m, c = map(int, input().split())
 
-distance = [INF] * (n+1)
 graph = [[] for _ in range(n+1)]
+
+distance = [INF] * (n+1)
 
 for _ in range(m):
     x, y, z = map(int, input().split())
     graph[x].append((y, z))
-    
-def search(start):
-    
-    q = []
-    
-    heapq.heappush(q, (0, start))
-    
-    distance[start] = 0
-    
-    while q:
-        
-        dist, now = heapq.heappop(q)
-        
-        if distance[now] < dist:
-            continue
-        
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-                
-search(c)
+
+q = []
+
+heapq.heappush(q, (0, c))
+
+distance[c] = 0
+
+for x in graph[c]:
+    distance[x[0]] = x[1]
+
+while q:
+
+    dist, now = heapq.heappop(q)
+
+    if distance[now] < dist:
+        continue
+
+    for x in graph[now]:
+        cost = dist + x[1]
+
+        if cost < distance[x[0]]:
+            distance[x[0]] = cost
+            heapq.heappush(q, (cost, x[0]))
 
 cnt = 0
-ma = 0
-
-for d in distance:
-    if d != INF:
+max_distance = -1
+for i in range(1, n+1):
+    if distance[i] != INF:
+        max_distance = max(max_distance, distance[i])
         cnt += 1
-        ma = max(ma, d)
 
-print(cnt-1, ma)
+print(cnt-1, max_distance)
 
 ## 예시 답안 ##
 
