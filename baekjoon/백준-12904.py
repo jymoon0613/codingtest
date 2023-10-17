fb33b36b5e5bcd83367c04affd90023151a23bde
jiyongmoon <asdwldyd123@gmail.com>
2023-10-17 21:29:21 +0900

    2023.10.17

---
+++ baekjoon/백준-12904.py
@@ -0,0 +1,20 @@
+# 12904: A와 B
+
+s = input()
+t = input()
+
+while True:
+
+    if len(s) == len(t):
+        break
+
+    if t[-1] == 'A':
+        t = t[:-1]
+    else:
+        t = t[:-1]
+        t = t[::-1]
+
+if s == t:
+    print(1)
+else:
+    print(0)
