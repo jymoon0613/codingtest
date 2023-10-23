6db8d51f37176c78ff3fa19fb835ed09d86e8768
jiyongmoon <asdwldyd123@gmail.com>
2023-10-23 17:18:07 +0900

    2023.10.23

--- baekjoon/백준-2141.py
+++ baekjoon/백준-2141.py
@@ -2,10 +2,18 @@
 
 n = int(input())
 
-X = []
-A = []
+total = 0
+XA = []
 for _ in range(n):
     x, a = map(int, input().split())
-    X.append(x)
-    A.append(a)
+    XA.append((x,a))
+    total += a
 
+XA.sort()
+
+now = 0
+for i in range(n):
+    now += XA[i][1]
+    if now >= total / 2:
+        print(XA[i][0])
+        break
