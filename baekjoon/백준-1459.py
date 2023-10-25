0397a473921ab759e50ef6cfa41cd18b3b0d6341
jiyongmoon <asdwldyd123@gmail.com>
2023-10-25 20:13:25 +0900

    2023.10.25

--- baekjoon/백준-1459.py
+++ baekjoon/백준-1459.py
@@ -3,40 +3,17 @@
 from collections import deque
 import copy
 
-tx, ty, w, s = map(int, input().split())
+x, y, w, s = map(int, input().split())
 
-MAP = [[0] * max(w,s) for _ in range(max(w,s))]
+res1 = (x + y) * w
 
-delta = [
-    [[-1, 1, 0, 0], [0, 0, -1, 1]],
-    [[-1, 1, 1, -1], [1, -1, 1, -1]]
-]
+if (x + y) % 2 == 0:
+    res2 = max(x,y) * s
+else:
+    res2 = (max(x,y) - 1) * s + w
 
-cost = [w, s]
+res3 = (min(x,y) * s) + ((max(x,y) - min(x,y)) * w)
 
-sx, sy = 0, 0
+result = min(res1, res2, res3)
 
-def search(array,sx,sy,mode):
-
-    q = deque()
-    q.append((sx,sy))
-
-    while q:
-
-        x, y = q.popleft()
-
-        if (x,y) == (tx,ty):
-            return array[x][y]
-
-        for i in range(4):
-            nx = x + delta[mode][0][i]
-            ny = y + delta[mode][1][i]
-
-            if nx >= 0 and nx < max(w,s) and ny >= 0 and ny < max(w,s):
-                if array[nx][ny] == 0 and (nx,ny) != (sx,sy):
-                    array[nx][ny] = array[nx][ny] + cost[mode]
-                    q.append((nx,ny))
-
-
-print(search(copy.deepcopy(MAP), 0, 0, 0))
-print(search(copy.deepcopy(MAP), 0, 0, 1))
+print(result)
